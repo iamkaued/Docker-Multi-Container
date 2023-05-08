c2f7173bdb63b48b19e7402b8f8083d97a4f963e
Kaue Eduardo <kaue.ke19@gmail.com>
2023-05-08 08:29:11 -0300

    Code updated.

--- backend/app.js
+++ backend/app.js
@@ -84,17 +84,19 @@ app.delete('/goals/:id', async (req, res) => {
   }
 });
 
-const MONGODB_URI = process.env.URL;
-mongoose
-  .connect(
-    MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (err) {
-        console.error('FAILED TO CONNECT TO MONGODB');
-        console.error(err);
-      } else {
-        console.log('CONNECTED TO MONGODB');
-        app.listen(80);
-      }
+mongoose.connect(
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.error('FAILED TO CONNECT TO MONGODB');
+      console.error(err);
+    } else {
+      console.log('CONNECTED TO MONGODB!!');
+      app.listen(80);
     }
-  );
+  }
+);
